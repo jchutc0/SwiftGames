982dbaf47f04560dc6350769b997e677e07f13c7
Casey Hutchinson <hutchster@gmail.com>
2023-10-28 22:08:11 -0400

    Implemented Side Scroll game

--- SideScroller/GameScene.swift
+++ SideScroller/GameScene.swift
@@ -10,82 +10,56 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+    var keypress = Keypress.arrowKeys()
+    var player: SKSpriteNode?
+    var map: SKTileMapNode?
+    let cameraBoundry = CGPoint(x: 250, y: 150)
+    let playerSpeed = CGFloat(5)
+        
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        player = childNode(withName: "player") as? SKSpriteNode
+        map = SKTileMapNode
+            .createInvisibleNodes(scene: self, tileMapName: "Walls")
+        if let player {
+            camera?.position = player.position
         }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
+    } // didMove
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func mouseDown(with event: NSEvent) {
-        self.touchDown(atPoint: event.location(in: self))
-    }
-    
-    override func mouseDragged(with event: NSEvent) {
-        self.touchMoved(toPoint: event.location(in: self))
-    }
-    
-    override func mouseUp(with event: NSEvent) {
-        self.touchUp(atPoint: event.location(in: self))
-    }
-    
-    override func keyDown(with event: NSEvent) {
-        switch event.keyCode {
-        case 0x31:
-            if let label = self.label {
-                label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-            }
-        default:
-            print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
-        }
-    }
-    
-    
+    override func keyUp(with event: NSEvent) { keypress.keyUp(event) }
+    override func keyDown(with event: NSEvent) { keypress.keyDown(event) }
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        if let pos = player?.position, let camera = camera {
+            if keypress.arrowPressed {
+                if keypress.leftArrow {
+                    player?.position.x = pos.x - playerSpeed
+                    if pos.x < camera.frame.origin.x - cameraBoundry.x {
+                        self.camera?.position.x = camera.position.x - playerSpeed
+                    }
+                    player?.zRotation = CGFloat.pi / 2
+                }
+                if keypress.rightArrow {
+                    player?.position.x = pos.x + playerSpeed
+                    if pos.x > camera.frame.origin.x + cameraBoundry.x {
+                        self.camera?.position.x = camera.position.x + playerSpeed
+                    }
+                    player?.zRotation = CGFloat.pi * 1.5
+                }
+                if keypress.downArrow {
+                    player?.position.y = pos.y - playerSpeed
+                    if pos.y < camera.frame.origin.y - cameraBoundry.y {
+                        self.camera?.position.y = camera.position.y - playerSpeed
+                    }
+                    player?.zRotation = CGFloat.pi
+                }
+                if keypress.upArrow {
+                    player?.position.y = pos.y + playerSpeed
+                    if pos.y > camera.frame.origin.y + cameraBoundry.y {
+                        self.camera?.position.y = camera.position.y + playerSpeed
+                    }
+                    player?.zRotation = 0
+                }
+            } // arrowPressed
+        }
     }
+
 }
